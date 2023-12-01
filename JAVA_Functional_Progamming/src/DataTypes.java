69412461f543ba3bd8eac9dee118893896229e35
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-30 21:57:19 -0800

    Data Types in Java.

---
+++ JAVA_Functional_Progamming/src/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // below are all primitive data types
+        // A primitive data type specifies the size and type of variable values, and it has no additional methods.
+        // there are total 8 primitive data types in JAVA.
+
+        byte myByte = 127;  // 1 byte	Stores whole numbers from -128 to 127
+
+        short myShort = 32767;  // 	2 bytes	Stores whole numbers from -32,768 to 32,767
+
+        int age = 20;   // 4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+
+        long price = 10L;   // 8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+        float myFloat = 50.0f;  // 4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+
+        double myDouble = 1.7;  // 	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+
+        char myChar = 'A'; // 2 bytes	Stores a single character/letter or ASCII values
+
+        boolean isTrue = false; // 	1 bit	Stores true or false values
+
+    }
+}
+
