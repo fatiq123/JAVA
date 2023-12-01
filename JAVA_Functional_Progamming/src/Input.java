dc263adbf14e7869913d9dcf13153ef5285af8ed
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-30 22:28:09 -0800

    Input in Java.

---
+++ JAVA_Functional_Progamming/src/Input.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        String userName = input.nextLine();
+
+        System.out.println("You entered: " + userName + "and length is: " + userName.length());
+
+        if (userName.length() < 8) {
+            System.out.println("Enter a new name: ");
+            String newName = input.nextLine();
+            System.out.println("You entered: " + newName);
+        } else {
+            System.out.println("Sorry you already have name greater then 8 characters");
+        }
+    }
+}
