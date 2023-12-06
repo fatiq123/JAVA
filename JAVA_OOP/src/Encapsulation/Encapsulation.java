61e181f76d5fc2ee9378e620f34563f4d252ca0c
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-12-05 22:37:53 -0800

    Encapsulation

---
+++ JAVA_OOP/src/Encapsulation/Encapsulation.java
@@ -0,0 +1,28 @@
+package Encapsulation;
+
+import java.util.Scanner;
+
+public class Encapsulation {
+
+    Scanner input = new Scanner(System.in);
+    private int deposit_money;
+
+
+    int calculate() {
+        return this.deposit_money;
+    }
+
+    void depositMoney(int salary) {
+        System.out.print("Enter the salary you want to deposit ");
+        salary = Integer.parseInt(input.nextLine());
+        this.deposit_money = salary;
+    }
+
+
+    public static void main(String[] args) {
+        Encapsulation e = new Encapsulation();
+        e.depositMoney(5000);
+        int total = e.calculate();
+        System.out.println("Total income is " + total);
+    }
+}
