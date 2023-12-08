447c36269fd1c036e421ebd2195aa49fcdea7fd1
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-12-07 22:12:20 -0800

    2d array in java

--- JAVA_Functional_Progamming/src/Collections/Array.java
+++ JAVA_Functional_Progamming/src/Collections/Array.java
@@ -4,10 +4,10 @@ public class Array {
     public static void main(String[] args) {
 
         int[] number = {1, 3, 5, 7, 9};
+        int[][] arr = new int[10][20];
+        arr[0][0] = 0;
+        System.out.println(arr[0][0]);
 
-        for (int num : number) {
-
-        }
 
         for (int n : number) {
             System.out.print(n);
