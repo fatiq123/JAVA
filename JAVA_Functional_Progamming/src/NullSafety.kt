9f68847d432c271ac47b28d01d9b6c385fc67159
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-12-22 22:50:17 -0800

    list in kotlin

--- JAVA_Functional_Progamming/src/NullSafety.kt
+++ JAVA_Functional_Progamming/src/NullSafety.kt
@@ -2,5 +2,6 @@ fun main(args: Array<String>) {
     val name = null
 //    val user? = null  // gives error
     val userName: String? = null
-
+    var list: List<Any>  = listOf("1",2,"3")
+    print(list)
 }
