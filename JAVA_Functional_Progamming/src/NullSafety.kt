2be6c345332a0f4b3883c1cc69c9c80b46d68a3d
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-12-24 20:14:53 -0600

    list in kotlin

--- JAVA_Functional_Progamming/src/NullSafety.kt
+++ JAVA_Functional_Progamming/src/NullSafety.kt
@@ -3,5 +3,8 @@ fun main(args: Array<String>) {
 //    val user? = null  // gives error
     val userName: String? = null
     var list: List<Any>  = listOf("1",2,"3")
+//    list[0] = 1 // gives error as we can not access listOf with indices
+    val indexList: List<Any> = mutableListOf("1",2,"3")
+    indexList[1]
     print(list)
 }
