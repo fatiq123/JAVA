a7bfeb4cfe33f553fb07a436b0ec081252319956
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2024-01-01 20:11:18 +0500

    Polymorphism in Java

--- JAVA_OOP/src/polymorphism/Calculator.java
+++ JAVA_OOP/src/polymorphism/Calculator.java
@@ -9,4 +9,14 @@ public class Calculator {
     double add(double a, double b) {
         return a + b;
     }
+
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(1,2));
+        System.out.println(c.add(1,2.0));
+    }
 }
+
+
